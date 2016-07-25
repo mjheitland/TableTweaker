6004dbf040ea77fa0c8a3d2365040b13014a2b36
dev-michaelheitland <michael.heitland@xero.com>
2016-07-25 16:31:33 +1200

    Not persisting layout any longer

--- TableTweaker/MainWindow.xaml.cs
+++ TableTweaker/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System.ComponentModel;
-using System.IO;
-using System.Windows;
-using System.Xml.Linq;
-using Xceed.Wpf.AvalonDock.Layout.Serialization;
 
 namespace TableTweaker
 {
@@ -20,9 +16,6 @@ namespace TableTweaker
             DataContext = viewModel;
             InitializeComponent();
             DocumentsPane.ToggleAutoHide();
-
-            LoadWindowLayout();
-            LoadDockLayout();
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -31,8 +24,6 @@ namespace TableTweaker
 
             if (!_isClosing)
             {
-                SaveDockLayout();
-                SaveWindowLayout();
                 Properties.Settings.Default.Save();
 
                 _isClosing = true;
@@ -43,52 +34,6 @@ namespace TableTweaker
             {
                 e.Cancel = !_isClosed;
             }
-        }
-
-        private void LoadWindowLayout()
-        {
-            var bounds = Properties.Settings.Default.WindowBounds;
-            if (bounds != new Rect())
-            {
-                Left = bounds.Left;
-                Top = bounds.Top;
-                Width = bounds.Width;
-                Height = bounds.Height;
-            }
-            var state = Properties.Settings.Default.WindowState;
-            if (state != WindowState.Minimized)
-            {
-                WindowState = state;
-            } 
-        }
-
-        private void SaveWindowLayout()
-        {
-            Properties.Settings.Default.WindowBounds = RestoreBounds;
-            Properties.Settings.Default.WindowState = WindowState;
-        }
-
-        private void LoadDockLayout()
-        {
-            var layout = Properties.Settings.Default.DockLayout;
-            if (string.IsNullOrEmpty(layout))
-                return;
-
-            var serializer = new XmlLayoutSerializer(DockingManager);
-            var reader = new StringReader(layout);
-            serializer.Deserialize(reader);
-        }
-
-        private void SaveDockLayout()
-        {
-            var serializer = new XmlLayoutSerializer(DockingManager);
-            var document = new XDocument();
-            using (var writer = document.CreateWriter())
-            {
-                serializer.Serialize(writer);
-            }
-            document.Root?.Element("FloatingWindows")?.Remove();
-            Properties.Settings.Default.DockLayout = document.ToString();
         }
     }
 }
--- TableTweaker/Properties/Settings.Designer.cs
+++ TableTweaker/Properties/Settings.Designer.cs
@@ -25,18 +25,6 @@ namespace TableTweaker.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool SendErrors {
-            get {
-                return ((bool)(this["SendErrors"]));
-            }
-            set {
-                this["SendErrors"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string LatestVersion {
             get {
@@ -44,42 +32,6 @@ namespace TableTweaker.Properties {
             }
             set {
                 this["LatestVersion"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0,0,0,0")]
-        public global::System.Windows.Rect WindowBounds {
-            get {
-                return ((global::System.Windows.Rect)(this["WindowBounds"]));
-            }
-            set {
-                this["WindowBounds"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string DockLayout {
-            get {
-                return ((string)(this["DockLayout"]));
-            }
-            set {
-                this["DockLayout"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Minimized")]
-        public global::System.Windows.WindowState WindowState {
-            get {
-                return ((global::System.Windows.WindowState)(this["WindowState"]));
-            }
-            set {
-                this["WindowState"] = value;
             }
         }
         
