a4ea6d6ba96b0036ee0162c8c79af0657d5c91f0
dev-michaelheitland <michael.heitland@xero.com>
2016-11-27 12:06:01 +1300

    off-by-one error in EncodeQuotationMark()

--- TableTweaker.Model/Engine.cs
+++ TableTweaker.Model/Engine.cs
@@ -203,7 +203,7 @@ namespace TableTweaker.Model
 
         private static string EncodeQuotationMark(string s)
         {
-            if (string.IsNullOrEmpty(s) || s.Length <= 2 || s[0] != '\"' || s[s.Length - 1] != '\"')
+            if (string.IsNullOrEmpty(s) || s.Length < 2 || s[0] != '\"' || s[s.Length - 1] != '\"')
                 throw new Exception($"Internal error, invalid args ({s ?? ""})");
 
             return $"\"{s.Substring(1, s.Length - 2).Replace("\"", "\\\"")}\"";
