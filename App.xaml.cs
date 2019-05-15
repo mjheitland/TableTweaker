4565a01f1e731a88e2cb021df8c44e6de2a817b7
mjheitland <mjheitland@gmail.com>
2019-05-15 10:33:11 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TableTweaker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
